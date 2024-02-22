88746052b82d4343f2476428148ebcf111497808
Diogo <ggdiogogg@gmail.com>
2024-02-22 10:58:03 -0300

    atualizando curso api-rest: controller post, put

--- BackEnd/APIs-with-NodeJS-express/modulo2-criando-API-REST-com-Express-e-MongoDB/src/controllers/livroController.js
+++ BackEnd/APIs-with-NodeJS-express/modulo2-criando-API-REST-com-Express-e-MongoDB/src/controllers/livroController.js
@@ -2,8 +2,21 @@ import livro from "../models/Livro.js";
 
 class LivroController {
   static async listarLivros(req, res) { //static permite acessar a função sem ter que instaciar a classe antes
-    const listaLivros = await livro.find({}); //acessando o model do mongoose
-    res.status(200).json(listaLivros);
+    try{
+      const listaLivros = await livro.find({}); //acessando o model do mongoose
+      res.status(200).json(listaLivros);
+    }catch{
+      res.status(500).json({message: `${erro.message} - falha na requisição`})
+    }
+  }
+  static async listarLivroPorId(req, res) { //static permite acessar a função sem ter que instaciar a classe antes
+    try{
+      const id = req.params
+      const livroEncontrado = await livro.findById(id)); //acessando o model do mongoose
+      res.status(200).json(livroEncontrado);
+    }catch{
+      res.status(500).json({message: `${erro.message} - falha na requisição do livro`})
+    }
   }
   static async cadastrarLivro(req,res){
     try{
@@ -11,6 +24,15 @@ class LivroController {
         res.status(201).json({message: "criado com sucesso", livro: novoLivro});
     }catch(erro){
         res.status(500).json({message: `${erro.message} - falha ao cadastrar livro`})
+    }
+  }
+  static async atualizarLivro(req, res) { //static permite acessar a função sem ter que instaciar a classe antes
+    try{
+      const id = req.params
+      await livro.findByIdAndUpdate(id, req.body); //
+      res.status(200).json({message: "livro atualizado"}});
+    }catch{
+      res.status(500).json({message: `${erro.message} - falha na atualização do livro`})
     }
   }
 }
